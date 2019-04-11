2a024b48e9b2e9017cc0fe4df4f5ad84df445e04
Pradnya Sonawane <43560888+Pradnya136@users.noreply.github.com>
2019-04-11 22:21:21 +0530

    Add files via upload

---
+++ Navigation.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Navigation = ({onRouteChange,issignedin})=> {
+	if (issignedin){
+		return(
+			<nav style = {{display:'flex', justifyContent:'flex-end'}}>
+			<p onClick= {()=> onRouteChange('Sign out')} className='f3 link dim white underline pa3 pointer'>Sign Out </p>
+			</nav>
+			);
+
+	}else{
+		return(
+				<nav style = {{display:'flex', justifyContent:'flex-end'}}>
+				<p onClick= {()=> onRouteChange('Signin')} className='f3 link dim white underline pa3 pointer'>Sign In </p>
+				<p onClick= {()=> onRouteChange('Register')} className='f3 link dim white underline pa3 pointer'>Register </p>
+				</nav>
+
+			);
+	}
+
+
+};
+
+
+export default Navigation; 
